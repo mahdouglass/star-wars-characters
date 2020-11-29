dca4769856692b4cc583e4bf4eb22ed68e6f3d7b
mahdouglass <mahdoulgass@gmail.com>
2020-11-28 22:50:21 -0500

    Added additional person information

--- src/components/person.component.js
+++ src/components/person.component.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const PersonComponent = ({person}) => {
     return (
-        <li>{person.name}</li>
+        <li>
+            {person.name}<br />
+            {person.height}<br />
+            {person.eye_color}<br />
+            {person.hair_color}<br />
+            {person.birth_year}
+        </li>
     )
 }
 
